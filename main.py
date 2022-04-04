0401c9058aaf2ec9ea4ba317b5333699f817eedc
Christopher Kumm <chrislkumm@protonmail.com>
2022-04-04 15:25:20 +0300

    Test Pillow

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
 if __name__ == "__main__":
-    # TODO: Make something.
-    pass
+    img = Image.open("StarWarsPlace.jpg")
+    print(img.format, img.size, img.mode)
+    img.show()
